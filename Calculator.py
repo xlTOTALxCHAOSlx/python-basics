5c50962ac7cb1b8ab075b39e89bf882671b539a0
xlTOTALxCHAOSlx <kalebtempest@gmail.com>
2015-01-10 17:11:54 +1000

    Calculator Working
    Working Not Full Complete

--- Calculator.py
+++ Calculator.py
@@ -6,29 +6,35 @@ def add(nb1, nb2):
     return nb1 + nb2
 #Subtract
 def sub(nb1, nb2):
-    return nb1 + nb2
+    return nb1 - nb2
 #Multply
 def mul(nb1, nb2):
-    return nb1 + nb2
+    return nb1 * nb2
 #Divide
 def div(nb1, nb2):
-    return nb1 + nb2
+    return nb1 / nb2
 
 def main():
-    mySym = input("operator: ")
-    if(operation != '+' and operation != '-' and operation != '*'and operation != '/')
+    mySym = input(": ")
+    if(mySym != '+' and mySym != '-' and mySym != '*'and mySym != '/'):
         #Invalid Operation
             print("Invalid Operation")
     else:
         myNum1 = int(input("First Number: "))
         myNum2 = int(input("Second Number: "))
         if(mySym == '+'):
-             print(add(myNum1, myNum2))
+            print(add(myNum1, myNum2))
+            return False
         elif(mySym == '-'):
              print(sub(myNum1, myNum2))
-        elif(mySym == '+'):
+             return False
+        elif(mySym == '*'):
              print(mul(myNum1, myNum2))
-        elif(mySym == '+'):
+             return False
+        elif(mySym == '/'):
              print(div(myNum1, myNum2))
-
-main()
+             return False
+while True : 
+    myPoo = main()
+    if myPoo == False:
+        break
